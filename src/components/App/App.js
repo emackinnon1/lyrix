d3f54aa2a85f73c078e60d127734ed89ccc0e55c
emackinnon1 <emackinnon100390@gmail.com>
2020-05-27 11:05:03 -0600

    Navbar is built out but need to figure out how to make active link

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import { Navbar } from "../Navbar/Navbar";
+
+const App = () => {
+	return (
+		<div className="App">
+			<Navbar />
+		</div>
+	);
+};
+
+export default App;
