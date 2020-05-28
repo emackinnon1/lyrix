cba6f72ff4883db931b7f1150c2b6006136a860e
Kyle Williams <kylecharleswilliams@gmail.com>
2020-05-28 16:54:42 -0600

    Add initial GameCard form functionality

--- src/components/GameCard/GameCard.js
+++ src/components/GameCard/GameCard.js
@@ -2,29 +2,40 @@ import React, { useState, useEffect } from "react";
 
 const GameCard = ({lyrics, updateCount}) => {
 
+    const [guesses, setGuesses] = useState({});
+    const [wordToReplace, setWordToReplace] = useState(-1);
+
     const createLyricLine = (line, index) => {
     const gameLyrics = line.split(' ');
-    const wordToReplace = Math.floor(Math.random() * gameLyrics.length);
+    
     const correctWord = gameLyrics[wordToReplace];
     const firstHalf = gameLyrics.splice(0, wordToReplace);
     const secondHalf = gameLyrics.splice(1);
     return (
         <p key={index}>
             {firstHalf.join(' ')}
-                <input type='text' className='input-box' key={index} onChange={(e)=> handleChange(e)} placeholder='Your Answer Here' />
+                <input type='text' className='input-box' key={index} id={index} onChange={(e) => handleChange(e)} placeholder='Your Answer Here' value={guesses}/>
             {secondHalf.join(' ')}
         </p>
-    )
+    )}
+
+    const checkWordToReplace = () => {
+        if(wordToReplace === -1){ 
+            setWordToReplace(Math.floor(Math.random() * gameLyrics.length));
+        }
     }
 
     const handleChange = (e) => {
-		const guess = e.target.value.toUpperCase();
-		const inputId = e.target.id
+        e.preventDefault();
+		setGuesses({index:e.target.value})
 		// setGuesses({...guesses, ['guess'+inputId]: guess});
 		// console.log('guesses state', guesses)
     }
-    const checkAnswers = () => {
+
+    const checkAnswers = (e) => {
+        e.preventDefault();
         updateCount();
+
     }
 
     const displayGameData = lyrics.map((line, index)=>{
@@ -32,23 +43,12 @@ const GameCard = ({lyrics, updateCount}) => {
     });
 
     return (
-    <div>
-            {displayGameData}
-
-        {/* <p>
-            {firstHalf1.join(' ')}
-            <input type='text' className='input-box' id={index} onChange={(e)=> handleChange(e)} placeholder='Your Answer Here' />
-            {secondHalf1.join(' ')}
-        </p>
-        <p>
-            {firstHalf2.join(' ')}
-            <input type='text' className='input-box' id={index} onChange={(e)=> handleChange(e)} placeholder='Your Answer Here' />
-            {secondHalf2.join(' ')}
-        </p> */}
+    <form onSubmit={(e) => checkAnswers(e)}>
+        {displayGameData}
         <div className='check-answers'>
-				<button onClick={() => checkAnswers()} className='next-btn'>NEXT</button>
+				<button type='submit' className='next-btn'>NEXT</button>
 		</div>
-    </div>)
+    </form>)
 }
 
 export default GameCard;
