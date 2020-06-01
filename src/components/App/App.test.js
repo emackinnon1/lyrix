c5f232e24854b19024b570290568bdd6e9f7d650
Brian Roxbury <roxburybrian@gmail.com>
2020-06-01 15:01:34 -0600

    Initial Testing setup for App

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,40 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
 import App from './App';
+import '@testing-library/jest-dom';
+import { MemoryRouter } from 'react-router-dom'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('App', () => {
+
+  it('should navigate to scores page', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+      
+        <App />
+       
+      </MemoryRouter>
+    );
+      
+    fireEvent.click(getByText('Scores'));
+    
+    const scorePage = getByText('Your scores:');
+    expect(scorePage).toBeInTheDocument();
+   
+    fireEvent.click(getByText('About'));
+
+  }) 
+
+  it('should display the rules when the app loads', () => {
+    const { getByText } = render(
+    <MemoryRouter>
+       <App />
+    </MemoryRouter>
+    );
+    
+    const rules = getByText('READ THE RULES FUCKHEAD');
+    expect(rules).toBeInTheDocument();
+
+    fireEvent.click(getByText('About'))
+  })
+}) 
