e0277d6c9e011b567b72f3f273dc6a2502ee4410
emackinnon1 <emackinnon100390@gmail.com>
2020-06-01 20:24:58 -0600

    Scores test is passing

--- src/components/Scores/Scores.test.js
+++ src/components/Scores/Scores.test.js
@@ -1,2 +1,17 @@
-import { React } from "react";
 import { Scores } from "./Scores";
+import React from "react";
+import { render } from "@testing-library/react";
+// import App from "./App";
+import "@testing-library/jest-dom";
+
+const mockScoreRecord = ["31/41", "25/50", "40/45"];
+
+describe("Scores", () => {
+	it("should render a user's scores", () => {
+		const { getByText, debug } = render(
+			<Scores scoreRecord={mockScoreRecord} />
+		);
+
+		expect(getByText("31/41")).toBeInTheDocument();
+	});
+});
