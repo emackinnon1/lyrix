6504d5c5a8bfa3783a8e57c346a966a5c5fbcf28
emackinnon1 <emackinnon100390@gmail.com>
2020-06-03 07:17:26 -0600

    Chart smoke test is passing

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,9 @@ export const About = () => {
 	return (
 		<div className="rules">
 			<h1>How to Play</h1>
-			<p>Click play on the navigation menu, find a song, and start guessing!</p>
+			<p>
+				Click "Play" on the navigation menu, find a song, and start guessing!
+			</p>
 		</div>
 	);
 };
